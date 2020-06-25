10cdb39443d72787877f532b50435ee9aba6a472
Sniddunc <sniddunc2@gmail.com>
2020-06-25 02:01:34 -0400

    :sparkles: Colored Logging

--- pkg/app/logging.go
+++ pkg/app/logging.go
@@ -3,6 +3,8 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/fatih/color"
 )
 
 // WrappedResponseWriter is a wrapped version of ResponseWriter which gives us access to the status code
@@ -23,5 +25,24 @@ func (wrw *wrappedResponseWriter) WriteHeader(statusCode int) {
 }
 
 func logRequest(c *Context, roundTime int64) {
-	log.Printf("| %d | %s %s | %dµs", c.Writer.statusCode, c.Request.Method, c.Request.URL.Path, roundTime)
+	statusCode := getStatusColor(c.Writer.statusCode).Sprintf(" %d ", c.Writer.statusCode)
+	requestMethod := color.New(color.Bold).Sprintf("%s", c.Request.Method)
+
+	log.Printf("%s %s %s | %dµs", statusCode, requestMethod, c.Request.URL.Path, roundTime)
+}
+
+func getStatusColor(statusCode int) *color.Color {
+	if statusCode >= 600 {
+		return color.New(color.BgBlack)
+	} else if statusCode >= 500 {
+		return color.New(color.BgRed)
+	} else if statusCode >= 400 {
+		return color.New(color.BgRed)
+	} else if statusCode >= 300 {
+		return color.New(color.BgCyan)
+	} else if statusCode >= 200 {
+		return color.New(color.BgGreen)
+	} else {
+		return color.New(color.BgBlack)
+	}
 }
